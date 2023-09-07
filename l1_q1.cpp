bf774c6dab73dee51ca67c011c476a9b18bf5090
Ana Caroline <109522583+carolribeiro0@users.noreply.github.com>
2023-09-07 19:06:31 -0300

    Add files via upload

---
+++ l1_q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int anos, meses, dias, dias_total;
+	
+	cout << "digite sua idade em anos, meses e dias: ";
+	cin>>anos >> meses >>dias;
+	
+	dias_total = ((anos*365) + (meses*30) + dias);
+	
+	cout <<"vc esta vivo a "<<dias_total<< " dias :)";
+}
